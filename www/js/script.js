6823e5b666c0ad187e69af3ad4e53a42b102b8e3
Shadow <silent.alex2010@yandex.ru>
2017-04-23 00:30:40 +0300

    newtest
    newtest once

---
+++ www/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ $('.bxslider').bxSlider({
+  mode: 'fade',
+  captions: true
+});//end slide toggle
+ 
+     $('#ex1').zoom();
+     $('#ex2').zoom();
+     $('#ex3').zoom();
+     $('#ex4').zoom();
+     $('#ex5').zoom();
+    
+});//end ready
